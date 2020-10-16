97446c0967da39fc6c355458647357578ec8c217
Zackery Steck <zackery.steck@etegent.com>
2020-10-16 16:40:52 -0400

    Initial commit for packagifying DETR

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="detr", # Replace with your own username
+    version="0.0.1",
+    author="Me",
+    author_email="author@example.com",
+    description="A small example package",
+    packages=setuptools.find_packages()
+)
